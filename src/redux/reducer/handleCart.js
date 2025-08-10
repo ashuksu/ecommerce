f4df3b69cb5fa2c61c06fa3844864bd30c3c736f
Ashuksu <ashuksu@gmail.com>
2025-08-10 14:49:19 +0200

    dev - Refactored cart handling logic and updated Navbar to display total items in cart

--- src/redux/reducer/handleCart.js
+++ src/redux/reducer/handleCart.js
@@ -1,41 +1,37 @@
-// Retrieve initial state from localStorage if available
-const getInitialCart = () => {
-  const storedCart = localStorage.getItem("cart");
-  return storedCart ? JSON.parse(storedCart) : [];
-};
+const cart = [];
 
-const handleCart = (state = getInitialCart(), action) => {
+const handleCart = (state = cart, action) => {
   const product = action.payload;
-  let updatedCart;
-
   switch (action.type) {
     case "ADDITEM":
-      // Check if product already in cart
       const exist = state.find((x) => x.id === product.id);
       if (exist) {
-        // Increase the quantity
-        updatedCart = state.map((x) =>
-          x.id === product.id ? { ...x, qty: x.qty + 1 } : x
+        return state.map((x) =>
+          x.id === product.id ? { ...x,
+            qty: x.qty + 1
+          } : x
         );
       } else {
-        updatedCart = [...state, { ...product, qty: 1 }];
+        const product = action.payload;
+        return [
+          ...state, {
+            ...product,
+            qty: 1,
+          },
+        ];
       }
-      // Update localStorage
-      localStorage.setItem("cart", JSON.stringify(updatedCart));
-      return updatedCart;
 
     case "DELITEM":
-      const exist2 = state.find((x) => x.id === product.id);
-      if (exist2.qty === 1) {
-        updatedCart = state.filter((x) => x.id !== exist2.id);
+      const exist1 = state.find((x) => x.id === product.id);
+      if (exist1.qty === 1) {
+        return state.filter((x) => x.id !== exist1.id);
       } else {
-        updatedCart = state.map((x) =>
-          x.id === product.id ? { ...x, qty: x.qty - 1 } : x
+        return state.map((x) =>
+          x.id === product.id ? { ...x,
+            qty: x.qty - 1
+          } : x
         );
       }
-      // Update localStorage
-      localStorage.setItem("cart", JSON.stringify(updatedCart));
-      return updatedCart;
 
     default:
       return state;
